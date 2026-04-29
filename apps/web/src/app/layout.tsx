dbe6eefba5be29e55756766e6074f66bd28e031b
정훈정훈아… <jeonghunjeonghun-a...@jeonghunjeonghun-aui-MacBookPro.local>
2026-04-29 22:00:39 +0900

    Add Naver WCS tracking script

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -50,6 +50,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MPKFHWTN');`,
           }}
         />
+        <script type="text/javascript" src="https://wcs.naver.net/wcslog.js" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.wcs_add = window.wcs_add || {};
+              window.wcs_add.wa = 's_4ba653e912f8';
+              window._nasa = window._nasa || {};
+              if (window.wcs) {
+                window.wcs.inflow();
+                window.wcs_do();
+              }
+            `,
+          }}
+        />
       </head>
       <body className="font-sans bg-white text-gray-900 antialiased overflow-x-hidden">
         {/* Google Tag Manager (noscript) */}
